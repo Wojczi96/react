a527023e8abc0c00b5705092b4924ebe80d90902
Wojciech Wyskiel <wojtek12.1.96@wp.pl>
2023-12-10 11:05:53 +0100

    Add Container components and styles

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,7 +1,7 @@
 import styles from './Button.module.scss';
 
-const Button = sente => {
-    return (<button className={styles.button}> {sente.text} </button>)
+const Button = props => {
+    return (<button className={styles.button}>{props.children}</button>)
 }
 
 export default Button;
