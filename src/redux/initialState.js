84436f5a1ef3c9c493a0eb20582e12f6082ea8fb
Wojciech Wyskiel <wojtek12.1.96@wp.pl>
2023-12-28 20:05:42 +0100

    Install redux and reorganisation of code

---
+++ src/redux/initialState.js
@@ -0,0 +1,28 @@
+const initialState = {
+    columns: [
+        {
+            id: 1,
+            title: 'Books',
+            icon: 'book',
+        },
+        {
+            id: 2,
+            title:'Movies',
+            icon:'film',
+        },
+        {
+            id:3,
+            title:'Games',
+            icon:'gamepad',
+        }
+    ],
+    cards:[
+        {id:1,columnId:1,title:'ThisisGoingtoHurt'},
+        {id:2,columnId:1,title:'InterpreterofMaladies'},
+        {id:3,columnId:2,title:'HarryPotter'},
+        {id:4,columnId:2,title:'StarWars'},
+        {id:5,columnId:3,title:'TheWitcher'},
+        {id:6,columnId:3,title:'Skyrim'}
+    ]
+};
+export default initialState;
