1ab00e5bf70ac09decf19a2aa6eb256463ddab1e
Wojciech Wyskiel <wojtek12.1.96@wp.pl>
2023-12-30 18:40:05 +0100

    Fix CardForm function

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -12,7 +12,7 @@ const Column = props => {
             <ul className={styles.cards}>
             {cards.map(card => <Card key={card.id} {...card} />)}
             </ul>
-        <CardForm columnId={props.id} action={props.addCard} />
+        <CardForm columnId={props.id} />
         </article>)
 }
 
