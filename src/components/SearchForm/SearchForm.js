e2f5a284da11e0b445245882f9d6c9f3c9e5065f
Wojciech Wyskiel <wojtek12.1.96@wp.pl>
2023-12-30 19:23:20 +0100

    Add new search function

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,10 +1,17 @@
 import styles from './SearchForm.module.scss';
 import TextInput from '../TextInput/TextInput';
 import Button from '../Button/Button';
+import { useDispatch } from 'react-redux';
 
 const SearchForm = () => {
+    const dispatch = useDispatch();
+    const handleSubmit = e => {
+        e.preventDefault();
+        dispatch({ type: 'UPDATE_SEARCHSTRING', payload: e.target[0].value})
+    }
+
     return (
-        <form className={styles.searchForm}>
+        <form className={styles.searchForm} onSubmit={handleSubmit}>
             <TextInput placeholder="Search..." />
             <div className={styles.searchButton}>
             <Button>
