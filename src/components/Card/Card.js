6bb50b58a0b1b33f1749762bf94e1b18790fd528
Wojciech Wyskiel <wojtek12.1.96@wp.pl>
2024-01-06 11:07:06 +0100

    Add to favorite function

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,8 +1,21 @@
+import clsx from 'clsx';
+import { useDispatch } from 'react-redux';
 import styles from './Card.module.scss';
+import { addFavorite } from '../../redux/store';
 
 const Card = props => {
+    const dispatch = useDispatch();
+    const handleSubmit = e => {
+        e.preventDefault();
+        dispatch(addFavorite(props.id));
+
+    }
     return (
-        <li className={styles.card}>{props.title}</li>
+        <li className={styles.card}>{props.title}
+            <div>
+                <button onClick={handleSubmit} className={styles.button}><i className={clsx("fa fa-star-o", props.isFavorite && styles.isFavorite)}></i></button>
+            </div>
+        </li>
     )
 }
 
--- src/components/Favorite/Favorite.js
+++ src/components/Favorite/Favorite.js
@@ -1,11 +1,24 @@
+import { useSelector } from "react-redux";
 import PageTitle from "../PageTitle/PageTitle";
+import { getAllFavorite } from "../../redux/store";
+import Card from "../Card/Card";
+import styles from "./Favorite.module.scss"
 
 
 
 const Favorite = () => {
+    const favorite = useSelector(state => getAllFavorite(state));
+
     return (
         <div>
             <PageTitle>FAVORITE</PageTitle>
+            <div className={styles.columnWrapper}>
+                <article className={styles.column}>
+                    <ul className={styles.cards}>
+                        {favorite.map(card => <Card key={card.id} title={card.title}/>)}
+                    </ul>
+                </article>
+            </div>
         </div>
     )
 }
